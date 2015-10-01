b05d71bc51c80d01c6634a347f302359d7f893ba
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-10-01 03:57:14 +0200

    Filling in the parser function, added some TODOs, added Checks folder (empty for now)

--- lib/Interpolate/Parse.hs
+++ lib/Interpolate/Parse.hs
@@ -10,8 +10,12 @@
 
 -- Created September 30 2015
 
--- TODO | -
---        -
+-- TODO | - Allow nested formats (?)
+--        - Allow interpolation of format parameters (?)
+--        - Polymorphic in String
+--        - Allow arbitrary monad transformers
+--        - Devise some way of specifying what parameters are valid for anyg iven FormatArg type
+--          -- Type-specific specifiers
 
 -- SPEC | -
 --        -
@@ -21,7 +25,7 @@
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- GHC Pragmas
 --------------------------------------------------------------------------------------------------------------------------------------------
-
+{-# LANGUAGE OverloadedStrings #-}
 
 
 
@@ -35,7 +39,10 @@ module Interpolate.Parse where
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- We'll need these
 --------------------------------------------------------------------------------------------------------------------------------------------
-import Text.Parsec
+import qualified Text.Parsec as Parsec
+import           Text.Parsec ((<?>), (<|>), ParsecT)
+import Data.String
+import Control.Monad.Identity
 
 import Interpolate.Types
 
@@ -44,3 +51,21 @@ import Interpolate.Types
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- Functions
 --------------------------------------------------------------------------------------------------------------------------------------------
+
+-- |
+-- parseformat :: IsString string => string -> a
+-- ParsecT s u m a is a parser with stream type s, user state type u, underlying monad m and return type a.
+-- TODO: Escaping '}'
+--                                              ↓                  ↓                   ↓                 ↓
+parseformat :: IsString s => ParsecT            s                  u               Identity        [FormatToken s i]
+parseformat = do
+  Parsec.many (plain <|> specifier)
+
+
+-- |
+plain :: IsString s => ParsecT s u Identity [FormatToken s i]
+plain = Parsec.noneOf "{}" <|> fmap fromString (Parsec.string "{{") <|> fmap fromString (Parsec.string "}}")
+
+-- |
+specifier :: IsString s => ParsecT s u Identity [FormatToken]
+specifier = error ""
